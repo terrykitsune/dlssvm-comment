057ea7350b7a026c8499271c24c14f1f4910f155
djosix <djosicks@gmail.com>
2017-04-12 23:54:49 +0800

    Fuck

--- DLSSVM/base_tracker/dlssvmOptimization.m
+++ DLSSVM/base_tracker/dlssvmOptimization.m
@@ -8,7 +8,7 @@
 %	e-mail:shaojiejiang@126.com, jf ning@sina.com
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [w0, patterns]=dlssvmOptimization(patterns,params, w0)
+function [w0, patterns] = dlssvmOptimization(patterns, params, w0)
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % parameters:
 %   patterns: all training set
@@ -21,29 +21,30 @@ function [w0, patterns]=dlssvmOptimization(patterns,params, w0)
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 P=5;
 % start=cputime;
-for p=1:P
-    n=size(patterns,2);
+for p = 1:P
+    n = size(patterns, 2);
     % select a sample to update its dual cofficient via DCD optimization
-    if p==1
-        idPat=n;
+    if p == 1
+        idPat = n;
     else
-        idPat = n - floor((p-1)*n/P);
+        idPat = n - floor((p-1)*n / P);
     end
 
-    patterns=updateWorkingSet(patterns,w0, idPat,1);
-    [w0, patterns]=updateOneAlpha(patterns,w0,params,idPat); % DCD optimizatoion
+    patterns = updateWorkingSet(patterns, w0, idPat, 1);
+    [w0, patterns] = updateOneAlpha(patterns, w0, params, idPat); % DCD optimizatoion
     
-    % maintian the budgets for support vectors 
-    svSize=getSVSize(patterns);
+    % maintian the budgets for support vectors
+    % remove support vector with the lowest weight until svSize == params.nBudget
+    svSize = getSVSize(patterns);
     while svSize > params.nBudget
-        [w0, patterns]=svBudgetMaintain(w0, patterns);
-        svSize=getSVSize(patterns);
+        [w0, patterns] = svBudgetMaintain(w0, patterns);
+        svSize = getSVSize(patterns);
     end
     
-    for q=1:10
-        n=size(patterns,2);
+    for q = 1:10
+        n = size(patterns,2);
         % select a sample to update its dual cofficient via DCD optimization
-        idPat = n - floor(n*(q-1)/10);
-        [w0, patterns]=updateOneAlpha(patterns,w0,params,idPat); % DCD optimization
+        idPat = n - floor(n*(q-1) / 10);
+        [w0, patterns] = updateOneAlpha(patterns, w0, params, idPat); % DCD optimization
     end
 end
--- DLSSVM/utils/svBudgetMaintain.m
+++ DLSSVM/utils/svBudgetMaintain.m
@@ -1,4 +1,4 @@
-function [w0, patterns]=svBudgetMaintain(w0, patterns)
+function [w0, patterns] = svBudgetMaintain(w0, patterns)
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % function: maintain the number of support vectors less than budgets.
 % parameters:
@@ -6,37 +6,41 @@ function [w0, patterns]=svBudgetMaintain(w0, patterns)
 %    patterns:
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-n=size(patterns,2);
-minWeight=1000;
+n = size(patterns,2);
+minWeight = 1000;
 
-for i=1:n
-    k=size(patterns{i}.supportVectorNum,2);
+% find the smallest weight of the support vectors
+% which is patterns{idPat}.supportVectorWeight(idSamp)
+for i = 1 : n
+    k = size(patterns{i}.supportVectorNum, 2);
     
-    if i==1 && k==1  
+    if i == 1 && k == 1
         continue;
     end
-    for j=1:k
-        w=patterns{i}.supportVectorWeight(j);
-        if w<minWeight
-            minWeight=w;
-            idPat=i;
-            idSamp=j;
+    for j = 1 : k
+        w = patterns{i}.supportVectorWeight(j);
+        if w < minWeight
+            minWeight = w;
+            idPat = i;
+            idSamp = j;
         end
     end
 end
 
-k=size(patterns{idPat}.supportVectorNum,2);
+k = size(patterns{idPat}.supportVectorNum, 2);
 
-if k<=1
+if k <= 1
+    % remove that pattern
     alpha = patterns{idPat}.supportVectorAlpha(1);
     num = patterns{idPat}.supportVectorNum(1);
     w0 = w0 - alpha * patterns{idPat}.X(num, :);
-    patterns(idPat)=[];
+    patterns(idPat) = [];
 else
+    % remove that support vector in the patterm
     alpha = patterns{idPat}.supportVectorAlpha(idSamp);
     num = patterns{idPat}.supportVectorNum(idSamp);
     w0 = w0 - alpha * patterns{idPat}.X(num, :);
-    patterns{idPat}.supportVectorNum(idSamp)=[];
-    patterns{idPat}.supportVectorAlpha(idSamp)=[];
-    patterns{idPat}.supportVectorWeight(idSamp)=[];
+    patterns{idPat}.supportVectorNum(idSamp) = [];
+    patterns{idPat}.supportVectorAlpha(idSamp) = [];
+    patterns{idPat}.supportVectorWeight(idSamp) = [];
 end
